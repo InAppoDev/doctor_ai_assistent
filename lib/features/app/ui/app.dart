a9552c02eb4c9f37bb303cd25a5ad5ba989976f4
k4zann <arshikoffice@gmail.com>
2024-12-05 18:59:33 +0500

    feat: get rid of unnecessary comments and dividing the widgets to different files

---
+++ lib/features/app/ui/app.dart
@@ -0,0 +1,18 @@
+import 'package:doctor_ai_assistent/features/home_page/presentation/pages/home_page.dart';
+import 'package:flutter/material.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'AI Doctor Assistent',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const HomePage()
+    );
+  }
+}
