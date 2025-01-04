dd04c51df28e23cc9ff95d930e62f78b256942e0
k4zann <arshikoffice@gmail.com>
2025-01-04 09:05:29 +0500

    feat: new datetime extension method + new package (calendar date picker)

--- lib/core/extensions/datetime_extension.dart
+++ lib/core/extensions/datetime_extension.dart
@@ -37,4 +37,60 @@ extension DateTimeExtension on DateTime {
         return 'Wrong month';
     }
   }
+
+  String getShortMonthName() {
+    switch (month) {
+      case 1:
+        return 'Jan';
+      case 2:
+        return 'Feb';
+      case 3:
+        return 'Mar';
+      case 4:
+        return 'Apr';
+      case 5:
+        return 'May';
+      case 6:
+        return 'Jun';
+      case 7:
+        return 'Jul';
+      case 8:
+        return 'Aug';
+      case 9:
+        return 'Sep';
+      case 10:
+        return 'Oct';
+      case 11:
+        return 'Nov';
+      case 12:
+        return 'Dec';
+      default:
+        return 'No data';
+    }
+  }
+
+  String getWeekDay() {
+    switch (weekday) {
+      case 1:
+        return 'Monday';
+      case 2:
+        return 'Tuesday';
+      case 3:
+        return 'Wednesday';
+      case 4:
+        return 'Thursday';
+      case 5:
+        return 'Friday';
+      case 6:
+        return 'Saturday';
+      case 7:
+        return 'Sunday';
+      default:
+        return 'No data';
+    }
+  }
+
+  String toUSAtimeString() {
+    return '${hour.toString().padLeft(2, '0')}:${minute.toString().padLeft(2, '0')} ${hour < 12 ? 'AM' : 'PM'}';
+  }
 }
